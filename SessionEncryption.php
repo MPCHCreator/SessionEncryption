9eace6601163ac8797db00de31d9a6afe07b8123
Miguel Pérez <miguelpercham@gmail.com>
2020-07-20 23:52:45 -0500

    Se agrego la encriptación de arrays y se ajusto el código

--- SessionEncryption.php
+++ SessionEncryption.php
@@ -5,21 +5,28 @@ include_once "EncryptionPHP/EncryptionPHP.php";
 class SessionEncryption{
 
     public static function sessionEncrypt($variableSession, $keyDirectory){
+        SessionEncryption::session($variableSession, $keyDirectory, "encode");
+    }
+
+    public static function sessionDecrypt($variableSession, $keyDirectory){
+        SessionEncryption::session($variableSession, $keyDirectory, "decode");
+    }
+
+    private static function session($variableSession, $keyDirectory, $methodName){
         # Obtenemos el nombre de la clave de la variable de sesion
         $variableName = array_keys($_SESSION, $variableSession);
         # Apuntamos al primer parametro
         $variableName = $variableName[0];
-        # ciframos el contenido de la variable de sesion
-        $sessionContent = EncryptionPHP::encode($variableSession, $keyDirectory);
+        # En caso de ser un arreglo encriptamos la informacion dentro de este
+        if(is_array($variableSession)){
+            for ($i=0; $i < count($variableSession); $i++) { 
+                $sessionContent[] = EncryptionPHP::$methodName($variableSession[$i], $keyDirectory);
+            }
+        }else{
+            # ciframos el contenido de la variable de sesion
+            $sessionContent = EncryptionPHP::$methodName($variableSession, $keyDirectory);
+        }
         # establecemos el contenido cifrado en la variable de sesion
         $_SESSION[$variableName] = $sessionContent;
-    }
-
-    public static function sessionDecrypt($variableSession, $keyDirectory){
-        $variableName = array_keys($_SESSION, $variableSession);
-        $variableName = $variableName[0];
-        # ciframos el contenido de la variable de sesion
-        $variableSession = EncryptionPHP::decode($variableSession, $keyDirectory);
-        $_SESSION[$variableName] = $variableSession;
     }
 }
--- example.php
+++ example.php
@@ -1,10 +1,12 @@
 <?php
-
 include_once "SessionEncryption.php";
+
 session_start();
 $keyDirectory = "clave.txt";
 $_SESSION['username'] = 'MPCH';
 $_SESSION['password'] = '1234';
+
+echo "<b>EJEMPLO DE STRING</b>"."<br>";
 # llamamos a la funcion sessionEncrypt para cifrar los datos de la variable de sesion
 # Establecemos los dos parametros (variable de sesion y el directorio de la llave)
 SessionEncryption::sessionEncrypt($_SESSION['password'], $keyDirectory);
@@ -13,3 +15,17 @@ echo "<br>";
 # llamamos a la funcion sessionDecrypt para descifrar los datos de la variable de sesion
 SessionEncryption::sessionDecrypt($_SESSION['password'], $keyDirectory);
 echo $_SESSION['password'];
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>EJEMPLO DE ARRAY</b>"."<br>";
+$array = ['uno','dos','tres','cuatro'];
+$_SESSION['mensaje'] = $array;
+
+SessionEncryption::sessionEncrypt($_SESSION['mensaje'], $keyDirectory);
+print_r($_SESSION['mensaje']);
+echo "<br>";
+SessionEncryption::sessionDecrypt($_SESSION['mensaje'], $keyDirectory);
+print_r($_SESSION['mensaje']);
+
