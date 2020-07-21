b4bf6b124ec96d67c198dcdd786983e9457afc27
Miguel Pérez <miguelpercham@gmail.com>
2020-07-21 14:56:55 -0500

    Se agrego la encriptación de objects y se ajusto el código

--- SessionEncryption.php
+++ SessionEncryption.php
@@ -17,14 +17,25 @@ class SessionEncryption{
         $variableName = array_keys($_SESSION, $variableSession);
         # Apuntamos al primer parametro
         $variableName = $variableName[0];
-        # En caso de ser un arreglo encriptamos la informacion dentro de este
-        if(is_array($variableSession)){
-            for ($i=0; $i < count($variableSession); $i++) { 
-                $sessionContent[] = EncryptionPHP::$methodName($variableSession[$i], $keyDirectory);
-            }
-        }else{
-            # ciframos el contenido de la variable de sesion
-            $sessionContent = EncryptionPHP::$methodName($variableSession, $keyDirectory);
+        # ciframos el contenido de la variable de sesion
+        switch ($variableSession) {
+            # Verificamos si es un arreglo
+            case is_array($variableSession):
+                for ($i=0; $i < count($variableSession); $i++) { 
+                    $sessionContent[] = EncryptionPHP::$methodName($variableSession[$i], $keyDirectory);
+                }
+                break;
+            # Verificamos si es un objeto
+            case is_object($variableSession):
+                foreach($variableSession as $clave => $valor) {
+                    $variableSession->$clave = EncryptionPHP::$methodName($valor, $keyDirectory);
+                }
+                $sessionContent = $variableSession;
+                break;
+            # Verificamos si es un string
+            case is_string($variableSession):
+                $sessionContent = EncryptionPHP::$methodName($variableSession, $keyDirectory);
+                break;
         }
         # establecemos el contenido cifrado en la variable de sesion
         $_SESSION[$variableName] = $sessionContent;
--- example.php
+++ example.php
@@ -20,12 +20,31 @@ echo "<br>";
 echo "<br>";
 
 echo "<b>EJEMPLO DE ARRAY</b>"."<br>";
-$array = ['uno','dos','tres','cuatro'];
-$_SESSION['mensaje'] = $array;
+$array = ['uno','dos','tres'];
+$_SESSION['array'] = $array;
 
-SessionEncryption::sessionEncrypt($_SESSION['mensaje'], $keyDirectory);
-print_r($_SESSION['mensaje']);
+SessionEncryption::sessionEncrypt($_SESSION['array'], $keyDirectory);
+print_r($_SESSION['array']);
 echo "<br>";
-SessionEncryption::sessionDecrypt($_SESSION['mensaje'], $keyDirectory);
-print_r($_SESSION['mensaje']);
+SessionEncryption::sessionDecrypt($_SESSION['array'], $keyDirectory);
+print_r($_SESSION['array']);
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>EJEMPLO DE OBJETO</b>"."<br>";
+class MiClase
+{
+    public $valor1 = 'uno';
+    public $valor2 = 'dos';
+    public $valor3 = 'tres';
+}
+
+$clase = new MiClase();
+$_SESSION['object'] = $clase;
+SessionEncryption::sessionEncrypt($_SESSION['object'], $keyDirectory);
+var_dump($_SESSION['object']);
+echo "<br>";
+SessionEncryption::sessionDecrypt($_SESSION['object'], $keyDirectory);
+var_dump($_SESSION['object']);
 
